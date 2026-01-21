b4c599d53a8106db638976dc8891f35cd507f62e
NazarK02 <nazarko2332@gmail.com>
2026-01-21 21:21:36 +0000

    ios test

--- ios/Podfile.rb
+++ ios/Podfile.rb
@@ -4,9 +4,10 @@ target 'Runner' do
   use_frameworks!
   use_modular_headers!
 
+  # Flutter pods
   flutter_install_all_ios_pods File.dirname(File.realpath(__FILE__))
 
-  # Force compatible pod versions
+  # Pin versions to resolve Firebase + MLKit conflict
   pod 'Firebase/CoreOnly', '11.15.0'
   pod 'FirebaseCore', '11.15.0'
   pod 'GoogleUtilities', '8.1.0'
