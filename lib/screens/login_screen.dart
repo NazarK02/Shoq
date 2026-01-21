d7909d11fb081ff5b872d59976e88ad95a7fea3d
NazarK02 <nazarko2332@gmail.com>
2026-01-21 08:45:57 +0000

    orginized folders

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'user_service.dart';
+import '../services/user_service.dart';
 import 'registration_screen.dart';
 
 class LoginScreen extends StatefulWidget {
